eb68c23634e7358db4046067f6e6ffdd72579c36
anabbenedetti <benedettianacb@gmail.com>
2023-07-04 22:45:14 -0300

    chore: add sitemap and robots.txt config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.renanrefrigeracao.com.br",
   integrations: [tailwind(), sitemap()]
 });
