9813cc2020627982ea416bb08d400f4f42046947
Lucas Mitchel <lucasmitchelcampos17@gmail.com>
2023-07-22 14:33:30 -0300

    feat(Icons): Add all icon for this Cards on Clients Section

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -5,3 +5,10 @@ export { default as Whatsapp } from "./defaultwhatsapp.svg";
 export { default as SocialInstagram } from "./social-instagram.png";
 export { default as SocialEmail } from "./social-email.png";
 export { default as SocialWhatsapp } from "./social-whatsapp.png";
+export { default as MarketIcon } from "./market.svg";
+export { default as HotelIcon } from "./hotel.svg";
+export { default as HouseIcon } from "./house.svg";
+export { default as ShopIcon } from "./shop.svg";
+export { default as CompanyIcon } from "./company.svg";
+export { default as IndustryIcon } from "./industry.svg";
+
