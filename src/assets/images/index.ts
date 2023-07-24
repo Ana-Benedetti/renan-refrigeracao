545078112af5a8f5031abf1f774aa1ae5c0ad762
Lucas Mitchel <lucasmitchelcampos17@gmail.com>
2023-07-24 13:34:33 -0300

    feat: Add instagram svg

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,7 +2,7 @@ export { default as Logo } from "./logo-rr.webp";
 export { default as LogoWhite } from "./logo-white-rr.webp";
 export { default as HeroBG } from "./hero-image.webp";
 export { default as Whatsapp } from "./defaultwhatsapp.svg";
-export { default as SocialInstagram } from "./social-instagram.png";
+export { default as SocialInstagram } from "./Instagram_icon.svg";
 export { default as SocialEmail } from "./social-email.png";
 export { default as SocialWhatsapp } from "./social-whatsapp.png";
 export { default as MarketIcon } from "./market.svg";
