67f82609883a956c3ed4d9ff5094191b4f491a6a
Guilherme Battoni <guilherme.battoni@gmail.com>
2023-07-26 12:30:58 -0300

    chore: Add correct settings for partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,14 @@ import partytown from '@astrojs/partytown';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.renanrefrigeracao.com.br',
-  integrations: [tailwind(), sitemap(), partytown()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
